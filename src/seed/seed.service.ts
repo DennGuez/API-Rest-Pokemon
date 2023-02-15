3f73abed66fbf462cd6958f0820cd1972c1be1ac
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-02-15 22:23:02 +0000

    FEAT: Insertar multiples registros simultaneamente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -11,23 +11,32 @@ export class SeedService {
     private readonly axios: AxiosInstance = axios
     
     constructor(
-      
       @InjectModel( Pokemon.name )
       private readonly pokemonModel: Model<Pokemon>
-      
     ){}
     
-    
     async executeSeed() {
-    const { data } =  await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
-    
-    data.results.forEach(async({ name, url}) => {
-      const segments = url.split('/')
-      const no = +segments[ segments.length - 2 ]
+      await this.pokemonModel.deleteMany({}) // delete * from pokemons;
+      const { data } =  await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+      // const insertPromisesArray = []
+      const pokemonToInsert: { name: string, no: number }[] = []
       
-      const pokemon = await this.pokemonModel.create( {name, no} )
-    })
-    
-    return 'Seed executed';
+      data.results.forEach(async({ name, url}) => {
+        const segments = url.split('/')
+        const no = +segments[ segments.length - 2 ]
+        
+        // const pokemon = await this.pokemonModel.create( {name, no} )
+        
+        // insertPromisesArray.push(
+        //   this.pokemonModel.create({ name, no})
+        // )
+        
+        pokemonToInsert.push({name, no})
+      })
+      
+      // await Promise.all( insertPromisesArray )
+      await this.pokemonModel.insertMany(pokemonToInsert)
+      
+      return 'Seed executed';
   }
 }
